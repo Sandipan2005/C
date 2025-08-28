838fc5eb570d318c708b00c674aa026d2a6036a1
Sandipan Majumder <sandipan.majumder.2005@outlook.com>
2025-08-28 19:38:06 +0530

    solve Q13

--- C-Basic-150.c
+++ C-Basic-150.c
@@ -271,26 +271,69 @@
 // Employees ID = 0342
 // Salary = U$ 120000.00
 
+// #include <stdio.h>
+// int main()
+// {
+//     int id,hr,amount;
+//     float salary;
+//     printf("Input the Employees ID(Max. 10 chars): ");
+//     scanf("%d",&id);
+//     printf("Input the working hrs: ");
+//     scanf("%d",&hr);
+//     printf("Salary amount/hr: ");
+//     scanf("%d",&amount);
+//
+//     salary = hr * amount;
+//
+//     printf("Employees ID = %d\n",id);
+//     printf("Salary = U$ %.2f",salary);
+// }
+
+
+// 13. Find the maximum of three integers (*)
+//
+// Write a C program that accepts three integers and finds the maximum of three.
+// Using Arithmetic Operations and abs() without using Ternary Operators & Loops.
+// Hint :- (a + b + abs(a - b)) / 2
+// Test Data :
+// Input the first integer: 25
+// Input the second integer: 35
+// Input the third integer: 15
+// Expected Output:
+// Maximum value of three integers: 35
+
+
+
 #include <stdio.h>
+#include <stdlib.h>
 int main()
 {
-    int id,hr,amount;
-    float salary;
-    printf("Input the Employees ID(Max. 10 chars): ");
-    scanf("%d",&id);
-    printf("Input the working hrs: ");
-    scanf("%d",&hr);
-    printf("Salary amount/hr: ");
-    scanf("%d",&amount);
+    int x, y, z, result, max; // Declare variables
 
-    salary = hr * amount;
+    // Prompt user for the first integer and store in 'x'
+    printf("\nInput the first integer: ");
+    scanf("%d", &x);
 
-    printf("Employees ID = %d\n",id);
-    printf("Salary = U$ %.2f",salary);
+    // Prompt user for the second integer and store in 'y'
+    printf("\nInput the second integer: ");
+    scanf("%d", &y);
+
+    // Prompt user for the third integer and store in 'z'
+    printf("\nInput the third integer: ");
+    scanf("%d", &z);
+
+    // Calculate the result
+    result = (x + y + abs(x - y)) / 2;
+
+    // Calculate the maximum value
+    max = (result + z + abs(result - z)) / 2;
+
+    // Print the maximum value
+    printf("\nMaximum value of three integers: %d", max);
+    printf("\n");
+
+    return 0;
 }
-
-
-
 
 
 

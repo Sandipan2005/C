7ccb533078ba4686bc66731db5f27199db729b92
Sandipan Majumder <sandipan.majumder.2005@outlook.com>
2025-08-28 19:14:57 +0530

    solve Q12

--- C-Basic-150.c
+++ C-Basic-150.c
@@ -259,8 +259,35 @@
 //     printf("Average Value = %f",average);
 // }
 
+// 12. Print employee ID and monthly salary
+//
+// Write a C program that accepts an employee's ID, total worked hours in a month and the amount he received per hour.
+// Print the ID and salary (with two decimal places) of the employee for a particular month.
+// Test Data :
+// Input the Employees ID(Max. 10 chars): 0342
+// Input the working hrs: 8
+// Salary amount/hr: 15000
+// Expected Output:
+// Employees ID = 0342
+// Salary = U$ 120000.00
 
+#include <stdio.h>
+int main()
+{
+    int id,hr,amount;
+    float salary;
+    printf("Input the Employees ID(Max. 10 chars): ");
+    scanf("%d",&id);
+    printf("Input the working hrs: ");
+    scanf("%d",&hr);
+    printf("Salary amount/hr: ");
+    scanf("%d",&amount);
 
+    salary = hr * amount;
+
+    printf("Employees ID = %d\n",id);
+    printf("Salary = U$ %.2f",salary);
+}
 
 
 
